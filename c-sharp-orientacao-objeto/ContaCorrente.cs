097aeadefd3f2cd02eeced813195942671921f7b
kaline <mesquitabfkaline@gmail.com>
2023-03-20 17:21:24 -0300

    Add project files.

---
+++ c-sharp-orientacao-objeto/ContaCorrente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_orientacao_objeto
+{
+    public class ContaCorrente
+    {
+        public int numero_Agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+    }
+}
